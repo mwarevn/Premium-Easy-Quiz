e46ecc0837367a574bc787c276f7dd7691615595
MWare <mwarevn@gmail.com>
2023-12-05 16:23:31 +0700

    update

--- injects/update_extension.js
+++ injects/update_extension.js
@@ -1,22 +1,18 @@
 const currentVersion = chrome.runtime.getManifest().version;
-var extensionVersion;
 
 fetch("https://6514b3f1dc3282a6a3cd7125.mockapi.io/server?name=Premium%20Easy%20Quiz")
     .then((res) => res.json())
     .then((res) => {
-        if (res.length === 1) {
-            extensionVersion = res[0].version;
-        }
-    })
-    .then(() => {
-        if (!currentVersion == extensionVersion) {
-            try {
+        if (res.length == 1) {
+            const lastestVersion = res[0].version;
+
+            if (currentVersion != lastestVersion) {
                 const body = document.querySelector("body");
                 body.innerHTML = `<h1 style="font-weight: bold; color: #303030">Premium Easy Quiz đã có phiên bản mới, vui lòng cài đặt phiên bản mới để tiếp tục
-                <br />
-                <a style="color: blue" target="_blank" href="https://github.com/mwarevn/Premium-Easy-Quiz"> Bấm vào đây để tải bản mới nhất</a>
-                </h1>
-                `;
-            } catch {}
+                    <br />
+                    <a style="color: blue" target="_blank" href="https://github.com/mwarevn/Premium-Easy-Quiz"> Bấm vào đây để tải bản mới nhất</a>
+                    </h1>
+                    `;
+            }
         }
     });
