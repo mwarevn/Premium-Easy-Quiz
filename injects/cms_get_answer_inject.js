5bcd280f49d50a509da6ffc1ba90e97f0ed38580
MWare <mwarevn@gmail.com>
2024-01-20 18:24:57 +0700

    @mwarevn
    update

--- injects/cms_get_answer_inject.js
+++
@@ -1,4 +0,0 @@
-var s = document.createElement("script");
-(s.src = chrome.runtime.getURL("injects/cms_get_answer.js")), (s.onload = () => s.parentNode.removeChild(s));
-let doc = document.documentElement || document.head;
-doc.appendChild(s);
--- popup/popup.js
+++
@@ -1,5 +0,0 @@
-document.querySelector("#btnLogin").addEventListener("click", function () {
-  chrome.runtime.sendMessage({ type: "login" }, function (e) {
-    console.debug(e), "success" == e && window.close();
-  });
-});
